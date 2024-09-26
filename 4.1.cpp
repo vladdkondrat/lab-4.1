9458e3313ca32e9d7f0821b3d207ac8101db536b
kondratvlad <vladkondrat87@gmail.com>
2024-09-26 20:33:01 +0300

    first master commit

---
+++ 4.1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int k, N;
+    double P;
+    cout << "k = "; cin >> k;
+    cout << "N = "; cin >> N;
+
+   
+    P = 1; // Починаємо з 1, оскільки це добуток
+    k = N; // Ініціалізуємо k, щоб почати з N
+    while (k <= 25)
+    {
+        P *= sqrt(1 + (N / k));
+        k++;
+    }
+    cout << P << endl;
+
+    P = 1; // Скидаємо P для нового обчислення
+    k = N; // Знову ініціалізуємо k для циклу do-while
+    do {
+        P *= sqrt(1 + (N / k));
+        k++;
+    } while (k <= 25);
+    cout << P << endl;
+
+
+    P = 1;
+    for (k = N; k <= 25; k++)
+    {
+        P *= sqrt(1 + (double)N / k);
+    }
+    cout << P << endl;
+
+    return 0;
+}
